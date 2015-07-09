58e2a1368501af5cd65a2cf9bcdfeb6c7cc82c8b
MaY0R <mayor@techie.com>
2015-07-09 04:33:57 +0200

    SpecialForces more special now!

--- Semesteraufgabe/OC2_Ananas_SemesterAufgabe/src/Common/CommonFunctions.java
+++ Semesteraufgabe/OC2_Ananas_SemesterAufgabe/src/Common/CommonFunctions.java
@@ -10,8 +10,10 @@ import jnibwapi.Position;
 import jnibwapi.Unit;
 import jnibwapi.JNIBWAPI;
 import jnibwapi.types.UnitType;
+import jnibwapi.types.UnitType.UnitTypes;
 import jnibwapi.util.BWColor;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
 
@@ -66,31 +68,16 @@ public class CommonFunctions {
 
     public static Position[] getEnemyHatcheries(JNIBWAPI bwapi){
         Position[] redHatcheries = new Position[] {new Position(516, 368), new Position(516, 2704)};
-        Position[] blueHatcheries = new Position[] {new Position(3420,368),  new Position(3420, 2704)};
-
+        Position[] blueHatcheries = new Position[] {new Position(3460,368),  new Position(3460, 2704)};
+        
         if(bwapi.getSelf().getID() == 0)
             return blueHatcheries;
         else
             return redHatcheries;
     }
 
-    public static Unit getClosestEnemyZergHatchery(Unit unit) {
-        Unit result = null;
-        double minDistance = Double.POSITIVE_INFINITY;
-
-        for (Unit enemy : AnanasAI.enemyUnits) {
-            double distance = getDistanceBetweenUnits(unit, enemy);
-            if (enemy.getType() == UnitType.UnitTypes.Zerg_Hatchery) {
-                if (distance < minDistance) {
-                    minDistance = distance;
-                    result = enemy;
-                }
-            }
-        }
-
-        return result;
-    }
-
+    
+    
     private Unit getClosestFriendlyZergHatchery(Unit unit) {
         Unit result = null;
         double minDistance = Double.POSITIVE_INFINITY;
